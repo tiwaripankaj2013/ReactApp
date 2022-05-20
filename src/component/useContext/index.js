04758e18b76938462df5dc2871fbc4a6cc54bc26
tiwaripankaj2013 <34410355+tiwaripankaj2013@users.noreply.github.com>
2022-05-20 09:54:49 +0530

    useContext hooks

--- src/component/useContext/index.js
+++ src/component/useContext/index.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import { contextData } from './contextData';
-import { UserList } from './userList';
+import React, { useState } from "react";
+import { contextData } from "./contextData";
+import { UserList } from "./userList";
 
- const UseContextHook = () => {
-
-  const [data,setData] = useState([
-    {id:'#1',name:'Pankaj Kumar',email:'pankaj@gmail.com'},
-    {id:'#2',name:'Prem Kumar',email:'rakesh@gmail.com'},
-    {id:'#3',name:'Praveen sharma',email:'prakash@gmail.com'},
-    {id:'#4',name:'Prakash Kumar',email:'shyam@gmail.com'},
-    {id:'#5',name:'Neha Singh',email:'neha@gmail.com'}
-  ])
+const UseContextHook = () => {
+  const [data, setData] = useState([
+    { id: "#1", name: "Pankaj Kumar", email: "pankaj@gmail.com" },
+    { id: "#2", name: "Prem Kumar", email: "rakesh@gmail.com" },
+    { id: "#3", name: "Praveen sharma", email: "prakash@gmail.com" },
+    { id: "#4", name: "Prakash Kumar", email: "shyam@gmail.com" },
+    { id: "#5", name: "Neha Singh", email: "neha@gmail.com" },
+  ]);
 
   return (
-  <contextData.Provider value={data}>
-   <UserList />
- </contextData.Provider>
-
-  )
-}
+    <>
+      <p>useContext hooks example</p>
+      <contextData.Provider value={data}>
+        <UserList />
+      </contextData.Provider>
+    </>
+  );
+};
 
 export default UseContextHook;
